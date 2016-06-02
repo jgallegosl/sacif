8b88720c74df562f76b6718f3fc87568e6dc0ae7
jgallegosl <jgallegosledon@gmail.com>
2016-06-02 12:00:25 -0500

    Commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetRow = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // btnGetRow
+            // 
+            this.btnGetRow.Location = new System.Drawing.Point(389, 168);
+            this.btnGetRow.Name = "btnGetRow";
+            this.btnGetRow.Size = new System.Drawing.Size(75, 23);
+            this.btnGetRow.TabIndex = 1;
+            this.btnGetRow.Text = "Obtener Fila Seleccionada";
+            this.btnGetRow.UseVisualStyleBackColor = true;
+            this.btnGetRow.Click += new System.EventHandler(this.btnGetRow_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(13, 13);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(117, 154);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 218);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.btnGetRow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetRow;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargaGrid();
+        }
+
+        private void CargaGrid()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Checked", typeof(Int32));
+            dt.Columns.Add("Nombre", typeof(string));
+            dt.Rows.Add(0, "ABC");
+            dt.Rows.Add(0, "DEF");
+            dt.Rows.Add(0, "HIJ");
+            dt.Rows.Add(0, "KLM");
+            dt.Rows.Add(0, "NOP");
+
+            ((ListBox)checkedListBox1).DataSource = dt;
+            ((ListBox)checkedListBox1).DisplayMember = "Nombre";
+            ((ListBox)checkedListBox1).ValueMember = "Checked";
+
+            string seleccionados = "Valores seleccionados: ";
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                bool isChecked = Convert.ToBoolean(Convert.ToInt32(dt.Rows[i]["Checked"].ToString()));
+                this.checkedListBox1.SetItemChecked(Convert.ToInt32(i), isChecked);
+                if (isChecked)
+                {
+                    seleccionados += string.Format("{0},", i.ToString());
+                }
+            }
+
+            //MessageBox.Show(seleccionados);
+        }
+
+        private void btnGetRow_Click(object sender, EventArgs e)
+        {
+            string seleccionados = "Valores seleccionados: ";
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox1.GetItemCheckState(i) == CheckState.Checked)
+                {
+                    DataRowView oDataRowView = checkedListBox1.Items[i] as DataRowView;
+                    seleccionados += string.Format("{0},", oDataRowView.Row["Nombre"].ToString());
+                }
+            }
+            MessageBox.Show(seleccionados);
+        }
+    }
+}
